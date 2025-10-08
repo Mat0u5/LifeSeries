7da0f734cfa797185c9d81b9559a25b1a02c136f
Mat0u5 <matous.danko+github@gmail.com>
2025-10-08 10:13:04 +0200

    Lying to the client about registries
    {skip_build}{skip_discord}

--- src/main/java/net/mat0u5/lifeseries/mixin/RegistrySyncManagerMixin.java
+++ src/main/java/net/mat0u5/lifeseries/mixin/RegistrySyncManagerMixin.java
@@ -1,23 +1,30 @@
 package net.mat0u5.lifeseries.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import it.unimi.dsi.fastutil.objects.Object2IntMap;
 import net.fabricmc.fabric.impl.registry.sync.RegistrySyncManager;
+import net.mat0u5.lifeseries.Main;
 import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Map;
 
-@Mixin(value = RegistrySyncManager.class, priority = 1)
+@Mixin(value = RegistrySyncManager.class, priority = 1, remap = false)
 public class RegistrySyncManagerMixin {
-/*
-    @Inject(method = "checkRemoteRemap", at = @At(value = "INVOKE", target = "Ljava/util/Map;isEmpty()Z"))
-    private static void checkRemoteRemap(Map<Identifier, Object2IntMap<Identifier>> map, CallbackInfo ci) {
-        if (map.size() == 1) {
-            ci.cancel();
+    @ModifyReturnValue(method = "createAndPopulateRegistryMap", at = @At(value = "RETURN"))
+    private static @Nullable Map<Identifier, Object2IntMap<Identifier>> checkRemoteRemap(@Nullable Map<Identifier, Object2IntMap<Identifier>> original) {
+        if (original != null) {
+            Identifier entityType = Identifier.ofVanilla("entity_type");
+            if (original.containsKey(entityType)) {
+                Object2IntMap<Identifier> entityTypes = original.get(entityType);
+                entityTypes.keySet().removeIf(value ->
+                        value.getNamespace().equalsIgnoreCase(Main.MOD_ID)
+                );
+                original.put(entityType, entityTypes);
+            }
         }
+        return original;
     }
-    */
 }
