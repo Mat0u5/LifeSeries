fa493b67b3d7ca2a732eea8e42be3e9dde588190
Mat0u5 <matous.danko+github@gmail.com>
2025-11-28 13:17:17 +0100

    1.20 resourcepacks

--- src/main/java/net/mat0u5/lifeseries/seasons/season/Season.java
+++ src/main/java/net/mat0u5/lifeseries/seasons/season/Season.java
@@ -61,14 +61,14 @@ import net.minecraft.world.level.GameRules;
 /*import net.minecraft.world.scores.DisplaySlot;*/
 
 public abstract class Season {
-    public static final String RESOURCEPACK_MAIN_URL = "https://github.com/Mat0u5/LifeSeries-Resources/releases/download/release-main-fc0fa2a3efe2aefdba5a3c0deda61039fc43a008/main.zip";
-    public static final String RESOURCEPACK_MAIN_SHA = "56d78f9818d17c461d00bee1cc505a0b2da96353";
+    public static final String RESOURCEPACK_MAIN_URL = "https://github.com/Mat0u5/LifeSeries-Resources/releases/download/release-main-27d9e98eb6009401319a5256f7695f1bba902412/main.zip";
+    public static final String RESOURCEPACK_MAIN_SHA = "6b3b1e6225156de119926dd32003060b42dbab82";
     public static final String RESOURCEPACK_SECRETLIFE_URL = "https://github.com/Mat0u5/LifeSeries-Resources/releases/download/release-secretlife-fc0fa2a3efe2aefdba5a3c0deda61039fc43a008/secretlife.zip";
     public static final String RESOURCEPACK_SECRETLIFE_SHA = "1befd668fa775f2b8715b348172e1ba776e57294";
-    public static final String RESOURCEPACK_MINIMAL_ARMOR_URL = "https://github.com/Mat0u5/LifeSeries-Resources/releases/download/release-minimal_armor-47b0d2488897145e7fd8b0b7da48033a097148e8/minimal_armor.zip";
-    public static final String RESOURCEPACK_MINIMAL_ARMOR_SHA = "3696f47350d675bae8b09b73163dca7051ac9bd6";
-    public static final String RESOURCEPACK_COMBINED_URL = "https://github.com/Mat0u5/LifeSeries-Resources/releases/download/release-combined-26a52a497854abf01508f6c230064c5d130f8622/combined.zip";
-    public static final String RESOURCEPACK_COMBINED_SHA = "e33174270f3b4c381b94181fb176448770e1115a";
+    public static final String RESOURCEPACK_MINIMAL_ARMOR_URL = "https://github.com/Mat0u5/LifeSeries-Resources/releases/download/release-minimal_armor-27d9e98eb6009401319a5256f7695f1bba902412/minimal_armor.zip";
+    public static final String RESOURCEPACK_MINIMAL_ARMOR_SHA = "e078d9085ea74891ebc4f8ec5686c4da08176a38";
+    public static final String RESOURCEPACK_COMBINED_URL = "https://github.com/Mat0u5/LifeSeries-Resources/releases/download/release-combined-27d9e98eb6009401319a5256f7695f1bba902412/combined.zip";
+    public static final String RESOURCEPACK_COMBINED_SHA = "d9ef5b0c1074c006820494ca0524de8f8810a77c";
 
     public int GIVELIFE_MAX_LIVES = 99;
     public boolean TAB_LIST_SHOW_DEAD_PLAYERS = true;
