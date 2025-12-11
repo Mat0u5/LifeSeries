23b21fba35cb67ddc22c11c62bc8e0fcbfd54322
Mat0u5 <matous.danko+github@gmail.com>
2025-12-11 20:48:47 +0100

    dev-1.4.5.7
    Snow now accumulates everywhere in layers, rather than randomly.
    Reduced snow textures even further.
    It no longer snows in the nether and end.

--- src/main/java/net/mat0u5/lifeseries/seasons/season/nicelife/NiceLife.java
+++ src/main/java/net/mat0u5/lifeseries/seasons/season/nicelife/NiceLife.java
@@ -3,10 +3,13 @@ package net.mat0u5.lifeseries.seasons.season.nicelife;
 import net.mat0u5.lifeseries.config.ConfigManager;
 import net.mat0u5.lifeseries.seasons.season.Season;
 import net.mat0u5.lifeseries.seasons.season.Seasons;
+import net.mat0u5.lifeseries.utils.other.OtherUtils;
+import net.mat0u5.lifeseries.utils.other.Time;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.ChunkPos;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LightLayer;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.block.Block;
@@ -14,15 +17,22 @@ import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.LiquidBlock;
 import net.minecraft.world.level.block.SnowLayerBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.dimension.DimensionType;
 import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
 
 import static net.mat0u5.lifeseries.Main.currentSession;
+import static net.mat0u5.lifeseries.Main.seasonConfig;
 
 public class NiceLife extends Season {
 
     public boolean LIGHT_MELTS_SNOW = false; //TODO CONFIG
+    public boolean SNOW_WHEN_NOT_IN_SESSION = false; //TODO CONFIG
+    public Time SNOW_LAYER_INCREASE_INTERVAL = Time.seconds(600); //TODO CONFIG
+    public Time snowTicks = Time.zero(); //TODO CONFIG
+    public double snowLayerTickChance = 1.0 / 48;
+    public int currentMaxSnowLayers = -1;
 
     @Override
     public Seasons getSeason() {
@@ -33,10 +43,30 @@ public class NiceLife extends Season {
     public ConfigManager createConfig() {
         return new NiceLifeConfig();
     }
+    @Override
+    public void reload() {
+        //SNOW_LAYER_INCREASE_INTERVAL = Time.seconds(Math.max(13, ...));
+        snowLayerTickChance = 250.0 / Math.max(SNOW_LAYER_INCREASE_INTERVAL.getTicks(), 1);
+        if (currentMaxSnowLayers == -1) {
+            currentMaxSnowLayers = seasonConfig.getOrCreateInt("current_snow_layers", 1);
+        }
+    }
 
     @Override
     public void tick(MinecraftServer server) {
         super.tick(server);
+        if (currentSession.statusStarted() || SNOW_WHEN_NOT_IN_SESSION) {
+            snowTicks.tick();
+            if (snowTicks.isLarger(SNOW_LAYER_INCREASE_INTERVAL)) {
+                snowTicks = Time.zero();
+                currentMaxSnowLayers++;
+                if (currentMaxSnowLayers > 8) {
+                    currentMaxSnowLayers = 1;
+                }
+                seasonConfig.setProperty("current_snow_layers", String.valueOf(currentMaxSnowLayers));
+                OtherUtils.log("Total Layers: "+currentMaxSnowLayers);
+            }
+        }
         long dayTime = server.overworld().getDayTime() % 24000L;
         /*
         boolean isNight =
@@ -46,13 +76,15 @@ public class NiceLife extends Season {
         server.overworld().setWeatherParameters(0, 1000, true, false);
     }
     public void tickChunk(ServerLevel level, ChunkPos chunkPos) {
-        if (!currentSession.statusStarted()) {//TODO config
+        if (level.dimension() != Level.OVERWORLD) {
             return;
         }
-        int minX = chunkPos.getMinBlockX();
-        int maxX = chunkPos.getMinBlockZ();
-        if (level.random.nextInt(48) == 0) {
-            customPrecipitation(level, level.getBlockRandomPos(minX, 0, maxX, 15));
+        if (currentSession.statusStarted()  || SNOW_WHEN_NOT_IN_SESSION) {
+            int minX = chunkPos.getMinBlockX();
+            int maxX = chunkPos.getMinBlockZ();
+            if (level.random.nextDouble() <= snowLayerTickChance) {
+                customPrecipitation(level, level.getBlockRandomPos(minX, 0, maxX, 15));
+            }
         }
     }
 
@@ -66,18 +98,23 @@ public class NiceLife extends Season {
 
         if (shouldSnow(level, topPos)) {
             BlockState state = level.getBlockState(topPos);
-            if (state.is(Blocks.SNOW)) {
-                int currentLayers = state.getValue(SnowLayerBlock.LAYERS);
-                if (currentLayers < 7) {
-                    BlockState newState = state.setValue(SnowLayerBlock.LAYERS, currentLayers + 1);
-                    Block.pushEntitiesUp(state, newState, level, topPos);
-                    level.setBlockAndUpdate(topPos, newState);
+            if (currentMaxSnowLayers > 0) {
+                if (state.is(Blocks.SNOW)) {
+                    int currentLayers = state.getValue(SnowLayerBlock.LAYERS);
+                    if (currentLayers < Math.min(currentMaxSnowLayers, 8)) {
+                        if (currentLayers == 7) {
+                            level.setBlockAndUpdate(topPos, Blocks.SNOW_BLOCK.defaultBlockState());
+                        }
+                        else {
+                            BlockState newState = state.setValue(SnowLayerBlock.LAYERS, currentLayers + 1);
+                            Block.pushEntitiesUp(state, newState, level, topPos);
+                            level.setBlockAndUpdate(topPos, newState);
+                        }
+                    }
                 }
-                else {
-                    level.setBlockAndUpdate(topPos, Blocks.SNOW_BLOCK.defaultBlockState());
+                else if (!(level.getBlockState(belowPos).is(Blocks.SNOW_BLOCK) && currentMaxSnowLayers == 8)) {
+                    level.setBlockAndUpdate(topPos, Blocks.SNOW.defaultBlockState());
                 }
-            } else {
-                level.setBlockAndUpdate(topPos, Blocks.SNOW.defaultBlockState());
             }
         }
 
