8b5f05ebc09903461c0dec10280c417eb4ba727a
Mat0u5 <matous.danko@gmail.com>
2026-09-01 17:33:47 +0200

    1.5.9.6-dev
    Updated to 26.3-pre-1

--- src/main/java/net/mat0u5/lifeseries/LifeSeries.java
+++ src/main/java/net/mat0u5/lifeseries/LifeSeries.java
@@ -38,7 +38,7 @@ import net.mat0u5.lifeseries.registries.ModRegistries;
 *///?}
 
 public class LifeSeries {
-	public static final String MOD_VERSION = "1.5.9.5-dev";
+	public static final String MOD_VERSION = "1.5.9.6-dev";
 	public static final String MOD_ID = "lifeseries";
 	private static final Platform PLATFORM = createPlatformInstance();
 
