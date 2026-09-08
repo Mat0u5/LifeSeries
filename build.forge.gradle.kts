f9d49caebe7f61cec5c60d6d4347dbbfdc1d2d58
Mat0u5 <matous.danko@gmail.com>
2026-09-08 13:52:40 +0200

    MatLib events pt2

--- build.forge.gradle.kts
+++ build.forge.gradle.kts
@@ -166,7 +166,7 @@ dependencies {
 	else {
 		compileOnly ("maven.modrinth:simple-voice-chat:forge-1.20.1-2.6.16")
 	}
-	implementation("net.mat0u5:matlib-forge-${stonecutter.current.version}:local-SNAPSHOT") { isChanging = true }
+	implementation("net.mat0u5:matlib-forge-${prop("deps.minecraft")}:local-SNAPSHOT") { isChanging = true }
 }
 
 if (legacyForge) {
